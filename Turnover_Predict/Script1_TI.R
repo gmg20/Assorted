7524405f8c3ee20cb3c7d5d1957e5f79e2395197
gmg20 <goodwing20@students.ecu.edu>
2021-11-16 21:11:24 -0500

    update

--- Turnover_Predict/Script1_TI.R
+++ Turnover_Predict/Script1_TI.R
@@ -1,92 +1,61 @@
 library(tidyverse)
 library(readr)
-library(readr)
 library(ROCR)
 library(caret)
-library(tidyverse)
 library(cvms)
 library(DescTools)
 library(class)
+library(ISLR)
 
-io1 <- read_csv("ProjectProspect/TI_Predict/IO_Level1_Data.csv")
-io1<-io1[-1]
+io1_filt <- read_csv("io1_filt2.csv")
+io1_filt<-io1_filt[,-c(1,3:4)]
+io1_filt$TI2<-as.factor(io1_filt$TI2)
 
-# Dichotomize TI into >3 Y/N
-io1$TI2<-cut(io1$TI,  c(0,3,5), labels=c("NoQuit","Quit"))
-io1<-io1 %>% relocate(TI2, .before = TI)
-table(io1$TI2)
-table(io1$code, io1$TI2)
-hist(io1$TI)
 
-# Eliminate non-key variables
-io1<-io1[,-c(7:9)]
-io1<-io1[,-8]
+set.seed(300)
+#Splitting data as training and test set. Using createDataPartition() function from caret
+indxTrain <- createDataPartition(y = io1_filt$TI2,p = 0.75,list = FALSE)
+training <- io1_filt[indxTrain,]
+testing <- io1_filt[-indxTrain,]
 
-# Write to CSV
-write.csv(io1, "io1_filt2.csv")
+#Checking distibution in orignal data and partitioned data
+prop.table(table(training$TI2)) * 100
+prop.table(table(testing$TI2)) * 100
+prop.table(table(io1$TI2)) * 100
 
-# Logistic Regression Model
-m1 <- glm(TI2 ~ . - code - TI, data = io1, family = binomial(link="logit"))
-summary(m1)
-cbind(OR = coef(m1), confint(m1))
-exp(cbind(OR = coef(m1), confint(m1)))
+###LOGREG USING CARET
+# Fit model (no tuning parameters for GLM)
+ctrl <- trainControl(method="repeatedcv",repeats = 9)
+logregFit <- train(TI2 ~ ., data = training, method = "glm", family="binomial",
+                trControl = ctrl, preProcess = c("center","scale"), tuneLength = 20)
 
-#Deviance
-m1n<-glm(TI2 ~ 1, data = io1, family = binomial(link = "logit"))
-anova(m1n, m1)
+#Output of logreg fit
+summary(logregFit)
 
-# Effect Size
-PseudoR2(m1, "McFadden")
-PseudoR2(m1, "Nagelkerke")
-PseudoR2(m1, "Tjur")
+# Predictions of LogReg
+logregPredict <- predict(logregFit, newdata = testing, type="prob")
+fitted.results<-predict(logregFit, newdata = testing)
 
-### Adding in Test/Train Splits
-
-# Split 
-index = sample(nrow(io1), size = (nrow(io1)*.7), replace = FALSE)
-io1_trn = io1[index,]
-io1_test = io1[-index,]
-
-# Remove TI continuous and Group Codes
-io1_trn<-io1_trn[,-c(2:3)]
-io1_test<-io1_test[,-c(2:3)]
-
-# Fit model with train
-m1trn = glm(TI2 ~ ., data=io1_trn, family = binomial(link='logit'))
-summary(m1trn)
-
-# Predict test data with model fitted on train
-fitted.results <- predict(m1trn,newdata=io1_test,type='response')
-fitted.results <- ifelse(fitted.results > 0.5,1,0)
-misClasificError <- mean(fitted.results != io1_test$TI2)
-print(paste('Accuracy',1-misClasificError))
-
-# ROC/AUC
-p <- predict(m1trn, newdata=io1_test, type="response")
-pr <- prediction(p, io1_test$TI2)
-prf <- performance(pr, measure = "tpr", x.measure = "fpr")
+# ROC/AUC for LogReg
+pr <- prediction(logregPredict[,2], testing$TI2)
+prf <- performance(pr, measure = "sens", x.measure = "fpr")
 plot(prf)
 auc <- performance(pr, measure = "auc")
 auc <- auc@y.values[[1]]
 auc
 
-# Confusion Matrix
-fitted.results <- predict(m1trn,newdata=io1_test, type='response')
-fitted.results <- ifelse(fitted.results > 0.5,1,0)
+#Confusion Matrix for LogReg
+
 fitted.results<-as.factor(fitted.results)
-misClasificError <- mean(fitted.results != io1_test$TI2)
+misClasificError <- mean(fitted.results != testing$TI2)
 print(paste('Accuracy',1-misClasificError))
 
-#METHOD A
-d <- tibble("target" = io1_test$TI2, "prediction" = fitted.results)
+d <- tibble("target" = testing$TI2, "prediction" = fitted.results)
 basic_d<-table(d)
 cfm <- as_tibble(basic_d)
-plot_confusion_matrix(cfm, 
-                      target_col = "target", 
-                      prediction_col = "prediction",
-                      counts_col = "n")
-# METHOD B
+
 conf_mat <- confusion_matrix(targets = d$target,predictions = d$prediction)
+
 plot_confusion_matrix(
   conf_mat$`Confusion Matrix`[[1]],
   font_counts = font(
@@ -101,3 +70,22 @@ plot_confusion_matrix(
     tc_tile_border_color = "black"
   )
 )
+
+# Visual ProbPred
+fitted.results = predict(logregFit, newdata = testing, type = "prob")
+head(fitted.results)
+bob<-data.frame(trueobs = testing$TI2, pred_prob_Q = fitted.results)
+bob$pred_prob_NoQ = (1-fitted.results)
+head(bob)
+
+ggplot(bob, aes(x = pred_prob_Q.Quit)) + 
+  geom_histogram(binwidth = .05) + 
+  facet_wrap(~trueobs) + 
+  xlab("Probability of Quit")
+
+# Accuracy 
+misClasificError <- mean(fitted.results != testing$TI2)
+print(paste('Accuracy',1-misClasificError))
+
+
+
